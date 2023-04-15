6c029e203b5068084ee8ab3b8212bc59c1371b9e
bruno <brunademais2012@hotmail.com>
2023-04-15 10:11:32 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const btnEl = document.getElementById("btn");
+const birthdayEl = document.getElementById("birthday");
+const resultEl = document.getElementById("result");
+
+function calculateAge() {
+    const birthdayValue = birthdayEl.value;
+    if (birthdayValue === "") {
+        alert("insira sua data de nascimento");
+    } else {
+        const age = getAge(birthdayValue);
+        resultEl.innerText = `Sua Idade É ${age} ${age > 1 ? "Anos" : "ano"} `;
+    }
+}
+
+function getAge(birthdayValue) {
+    const currentDate = new Date();
+    const birthdayDate = new Date(birthdayValue);
+    let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+    const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+    if (
+        month < 0 ||
+        (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+    ) {
+        age--;
+    }
+
+    return age;
+}
+
+btnEl.addEventListener("click", calculateAge);
+
+btnEl.addEventListener("click", calculateAge);
